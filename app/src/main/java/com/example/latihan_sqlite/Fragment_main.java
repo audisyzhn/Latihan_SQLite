dbb2d892bb859b1a80a690008e50a76644628cdf
audisyzhn <audi17042002@gmail.com>
2019-09-05 11:28:50 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/latihan_sqlite/Fragment_main.java
@@ -0,0 +1,14 @@
+package com.example.latihan_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+    }
+}
